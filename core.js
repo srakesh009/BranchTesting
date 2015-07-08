b6d2b8005d42bed9d9620a46dc45cfc9de902c12
srakesh <Rakesh.Shashidhara@bendigoadelaide.com.au>
2015-07-08 21:30:54 +1000

    My first commit

---
+++ core.js
@@ -0,0 +1,129 @@
+'use strict';
+
+//angular.module('comsApp.services', [])
+
+/**
+ * @service Configuration
+ * environments are defined in environmentSettings.js
+ * sets "global" constant configuration  settings
+ * @return {Object} configModel
+ */
+    .factory("Configuration", function ($location,EnvironmentConfiguration) {
+        var configModel = {};
+
+        for (var env in EnvironmentConfiguration.environments) {
+
+            if (EnvironmentConfiguration.environments[env].hostNames.indexOf($location.host().toUpperCase()) != -1) {
+
+                angular.extend(configModel, EnvironmentConfiguration.environments[env]);
+            }
+        }
+        return configModel;
+    })
+/**
+ * Central Web Service call function
+ */
+    .factory("WebService", function ($http, $log) {
+        return {
+            /**
+             * TODO Add JSDoc comment.
+             *
+             * @param {String} request
+             * @param {Object} parameters
+             */
+            send: function (request, parameters) {
+                if (parameters == undefined || parameters == null) {
+                    return;
+                }
+
+                if (parameters.onError == undefined || parameters.onError == null) {
+                    parameters.onError = function (data, status, headers, config) {
+                        $log.error("comsApp.services.WebService.send: HTTP ERROR [ METHOD: " + config.method + ", STATUS: " + status + ", URL: " + config.url + " ]");
+                        // TODO : swap these and change the test to succeed
+                      //  $log.info(angular.toJson(config.data));
+                     //   $log.info(angular.toJson(data));
+
+                    };
+                }
+
+                return $http.post(parameters.to, request).success(parameters.onSuccess).error(parameters.onError);
+            }
+        };
+    })
+/**
+ * Base 64 Function
+ */
+    .factory('Base64', function () {
+        var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        return {
+            encode: function (input) {
+                var output = "", chr1, chr2, chr3 = "",enc1, enc2, enc3, enc4 = "",i = 0;
+
+                do {
+                    chr1 = input.charCodeAt(i++);
+                    chr2 = input.charCodeAt(i++);
+                    chr3 = input.charCodeAt(i++);
+
+                    enc1 = chr1 >> 2;
+                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                    enc4 = chr3 & 63;
+
+                    if (isNaN(chr2)) {
+                        enc3 = enc4 = 64;
+                    } else if (isNaN(chr3)) {
+                        enc4 = 64;
+                    }
+
+                    output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+                } while (i < input.length);
+
+                return output;
+            },
+
+            decode: function (input) {
+                var output = "",chr1, chr2, chr3 = "",enc1, enc2, enc3, enc4 = "",i = 0;
+
+                // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+                var base64test = /[^A-Za-z0-9\+\/\=]/g;
+                if (base64test.exec(input)) {
+                    alert("There were invalid base64 characters in the input text.\n" +
+                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                        "Expect errors in decoding.");
+                }
+                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+                do {
+                    enc1 = keyStr.indexOf(input.charAt(i++));
+                    enc2 = keyStr.indexOf(input.charAt(i++));
+                    enc3 = keyStr.indexOf(input.charAt(i++));
+                    enc4 = keyStr.indexOf(input.charAt(i++));
+
+                    chr1 = (enc1 << 2) | (enc2 >> 4);
+                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                    chr3 = ((enc3 & 3) << 6) | enc4;
+
+                    output = output + String.fromCharCode(chr1);
+
+                    if (enc3 != 64) {
+                        output = output + String.fromCharCode(chr2);
+                    }
+                    if (enc4 != 64) {
+                        output = output + String.fromCharCode(chr3);
+                    }
+
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+
+                } while (i < input.length);
+
+                return output;
+            }
+        };
+    });
---
+++ documentum.js
@@ -0,0 +1,102 @@
+'use strict';
+
+/**
+ * TODO Add JSDoc comment.
+ */
+angular.module("comsApp.services").factory("Documentum", function (WebService, Configuration, Templates,$log) {
+
+    /**
+     * TODO Add JSDoc comment.
+     *
+     * @param {String} body
+     * @return {String}
+     */
+    var request = function(body) {
+        // User.checkAuthUser();
+            return Templates.DOCUMENTUM_SOAP_ENVELOPE.replace(/\{userName\}/g, Configuration.DCTM_LOGIN )
+                                                     .replace(/\{password\}/g, Configuration.DCTM_PASS )
+                                                     .replace(/\{repository\}/g, Configuration.DCTM_REPOSITORY )
+                                                     .replace(/\{body\}/g, body);
+    };
+
+
+
+    return {
+
+        dctmProperties : function(data , obj){
+            // handle successful data or leave alone
+            if (!data.Envelope.Body.executeResponse['return'].dataPackage.DataObjects ) {
+                return data;
+            }else{
+                data = data.Envelope.Body.executeResponse['return'].dataPackage.DataObjects
+            }
+            for (var i in data){
+                obj[i] = {}
+                if (data[i].Identity){
+                    if (data[i].Identity.ObjectId._id){
+                        obj[i].id= data[i].Identity.ObjectId._id;
+                    }
+                }
+                if (data[i].Properties){
+                    if (data[i].Properties.Properties){
+                        for (var j in data[i].Properties.Properties){
+                            for (var k in data[i].Properties.Properties[j]){
+                                for (var item in data[i].Properties.Properties[j][k]) {
+                                    if  (item == "__text"){
+                                        obj[i][data[i].Properties.Properties[j]._name]=data[i].Properties.Properties[j][k].__text ;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        },
+
+        /**
+         * @function getDocumentFromId
+         *
+         * @param {String}   objectId
+         * @param {Function} [success]
+         * @param {Function} [error]
+         * @return {Object}                     The promise returned by the WebService service.
+         */
+        getDocumentFromId: function(objectId, success, error) {
+
+           // $log.info("getDocumentFromId "+ objectId)
+
+            var message = Templates.GET_OBJECT_REQUEST.replace(/\{objectId\}/g, objectId)
+                                                      .replace(/\{repository\}/g, Configuration.DCTM_REPOSITORY );
+
+            // $log.info(request(message))
+
+            return WebService.send(request(message), {
+                to:         Configuration.DCTMSERVICE_URL_OBJECT,
+                onSuccess:  success,
+                onError:    error
+            });
+        },
+
+        /**
+         * TODO Add JSDoc comment.
+         *
+         * @param {String}   query
+         * @param {Function} [success]
+         * @param {Function} [error]
+         * @return {Object}             The promise returned by the WebService service.
+         */
+        query: function(query, success, error) {
+            var message = Templates.QUERY_REPOSITORY.replace(/\{query\}/g, query)
+                                                    .replace(/\{repository\}/g, Configuration.DCTM_REPOSITORY );
+
+            return WebService.send(request(message), {
+                to:         Configuration.DCTMSERVICE_URL_QUERY,
+                onSuccess:  success,
+                onError:    error
+            });
+        }
+
+
+    }
+});
